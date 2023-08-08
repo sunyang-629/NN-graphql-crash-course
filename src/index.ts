f223bfd410887949141e72f3967a6164dcd4d76a
Yang Sun <sunyang9876@hotmail.com>
2023-08-08 22:32:20 +1000

    5-query-variables

--- src/index.ts
+++ src/index.ts
@@ -8,11 +8,20 @@ const resolvers = {
     games() {
       return db.games;
     },
+    game(_parent, args) {
+      return db.games.find((game) => game.id === args.id);
+    },
     authors() {
       return db.authors;
     },
+    author(_parent, args) {
+      return db.authors.find((author) => author.id === args.id);
+    },
     reviews() {
       return db.reviews;
+    },
+    review(_parent, args) {
+      return db.reviews.find((review) => review.id === args.id);
     },
   },
 };
