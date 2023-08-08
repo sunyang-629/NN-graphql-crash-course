a5ce548c6e2d3a413993d80e9864bb7d792707ea
Yang Sun <sunyang9876@hotmail.com>
2023-08-08 22:17:07 +1000

    4-run-server

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,26 @@
 import { ApolloServer } from "@apollo/server";
 import { startStandaloneServer } from "@apollo/server/standalone";
+import { typeDefs } from "./schema.js";
+import db from "./_db.js";
 
-import { typeDefs } from "./schema";
+const resolvers = {
+  Query: {
+    games() {
+      return db.games;
+    },
+    authors() {
+      return db.authors;
+    },
+    reviews() {
+      return db.reviews;
+    },
+  },
+};
 
 //server setup
 const server = new ApolloServer({
   typeDefs,
-  // resolvers
+  resolvers,
 });
 
 const { url } = await startStandaloneServer(server, { listen: { port: 4000 } });
